ddd8026619936d04bba5f8a8f5c7cfe59826fff8
Oliver Silva <silva.liver@gmail.com>
2025-03-04 13:10:24 -0300

    test: remove variavel

--- resources/js/__tests__/formAddLocalHelper.test.js
+++ resources/js/__tests__/formAddLocalHelper.test.js
@@ -4,13 +4,10 @@ import { data } from "../helpers/formAddLocalHelper";
 global.$q = {
     notify: jest.fn(),
 };
-global.onMounted = (callback) => {
-    callback();
-}
 describe("No arquivo formAddLocalHelper,", () => {
     test("o método data deve retornar o objeto contendo os dados do formulario, vazios por padrão", () => {
         const result = data($q);
-        
+
         expect(result.nome).toBe('');
         expect(result.email).toBe('');
         expect(result.slug).toBe('');
